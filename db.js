75754979c75504664ffee83124e96d434f1cab27
ian-Liaozy <zl3057@nyu.edu>
2023-07-27 21:11:39 +0800

    before redeploy - 2

--- db.js
+++ db.js
@@ -18,22 +18,22 @@ const News = new mongoose.Schema({
 });
 
 let dbconf;
-if (process.env.NODE_ENV === 'PRODUCTION') {
-	// if we're in PRODUCTION mode, then read the configration from a file
-	// use blocking file io to do this...
-	const fs = require('fs');
-	const path = require('path');
-	const fn = path.join(__dirname, 'config.json');
-	const data = fs.readFileSync(fn);
+// if (process.env.NODE_ENV === 'PRODUCTION') {
+// 	// if we're in PRODUCTION mode, then read the configration from a file
+// 	// use blocking file io to do this...
+// 	const fs = require('fs');
+// 	const path = require('path');
+// 	const fn = path.join(__dirname, 'config.json');
+// 	const data = fs.readFileSync(fn);
 
-	// our configuration file will be in json, so parse it and set the
-	// conenction string appropriately!
-	const conf = JSON.parse(data);
-	dbconf = conf.dbconf;
-} else {
-	// if we're not in PRODUCTION mode, then use
-	dbconf = 'mongodb+srv://zl3057:ian@cluster0.4usnwgg.mongodb.net/';
-}
+// 	// our configuration file will be in json, so parse it and set the
+// 	// conenction string appropriately!
+// 	const conf = JSON.parse(data);
+// 	dbconf = conf.dbconf;
+// } else {
+// 	// if we're not in PRODUCTION mode, then use
+dbconf = 'mongodb+srv://zl3057:ian@cluster0.4usnwgg.mongodb.net/';
+// }
 // Keyword.plugin(passportLocalMongoose);
 // News.plugin(URLSlugs('name'));
 
