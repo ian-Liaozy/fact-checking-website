3d0068b978e545b2a6aa46049001b5063237c05a
ian-Liaozy <zl3057@nyu.edu>
2023-07-28 00:17:43 +0800

    deploy to vercel

--- app.js
+++ app.js
@@ -10,36 +10,32 @@ const keywords = require('./routes/keywords.js');
 const news = require('./routes/news.js');
 const session = require('express-session');
 
-function runApp() {
-    app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 
-    // view engine setup
-    // app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'hbs');
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-    // enable sessions
-    const sessionOptions = {
-        secret: 'secret cookie thang (store this elsewhere!)',
-        resave: true,
-        saveUninitialized: true
-    };
-    app.use(session(sessionOptions));
-    app.use(express.static(path.join(__dirname, 'public')));
+// enable sessions
+const sessionOptions = {
+    secret: 'secret cookie thang (store this elsewhere!)',
+    resave: true,
+    saveUninitialized: true
+};
+app.use(session(sessionOptions));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-    app.use('/', routes);
-    app.use('/search', routes);
-    app.use('/keywords', keywords);
-    app.use('/news', news);
+app.use('/', routes);
+app.use('/search', routes);
+app.use('/keywords', keywords);
+app.use('/news', news);
 
-    app.listen(process.env.PORT, () => {
-        console.log('Server is running on port 3001');  
-    });
-}
+app.listen(3001);
 
 
 // app.listen(process.env.PORT || 3001);
 
 // exports.app = app;
-exports.default = runApp;
+module.exports = app
